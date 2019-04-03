936e80a38e3aa1cb48fb5e60fb6749f456bbe626
Eric Bernardo <eric.bernardo.sousa@gmail.com>
2019-04-02 21:45:22 -0300

    Auth user

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -6,31 +6,31 @@ import { Storage } from '@ionic/storage';
 import { environment } from '../../environments/environment';
 import { tap, catchError } from 'rxjs/operators';
 import { BehaviorSubject } from 'rxjs';
- 
+
 const TOKEN_KEY = 'access_token';
- 
+
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
- 
+
   url = environment.url;
   user = null;
   authenticationState = new BehaviorSubject(false);
- 
+
   constructor(private http: HttpClient, private helper: JwtHelperService, private storage: Storage,
     private plt: Platform, private alertController: AlertController) {
     this.plt.ready().then(() => {
       this.checkToken();
     });
   }
- 
+
   checkToken() {
-    this.storage.get(TOKEN_KEY).then(token => {      
+    this.storage.get(TOKEN_KEY).then(token => {
       if (token) {
-        let decoded = this.helper.decodeToken(token);
-        let isExpired = this.helper.isTokenExpired(token);
- 
+        const decoded = this.helper.decodeToken(token);
+        const isExpired = this.helper.isTokenExpired(token);
+
         if (!isExpired) {
           this.user = decoded;
           this.authenticationState.next(true);
@@ -44,7 +44,7 @@ export class AuthService {
   login(credentials) {
     return this.http.post(`${this.url}auth`, credentials)
       .pipe(
-        tap(res => {          
+        tap(res => {
           this.storage.set(TOKEN_KEY, res['access_token']);
           this.user = this.helper.decodeToken(res['access_token']);
           this.authenticationState.next(true);
@@ -56,18 +56,18 @@ export class AuthService {
 
       );
   }
- 
+
   logout() {
     this.storage.remove(TOKEN_KEY).then(() => {
       this.authenticationState.next(false);
     });
   }
- 
+
   getSpecialData() {
     return this.http.get(`${this.url}me`).pipe(
       catchError(e => {
         let status = e.status;
-        if (status === 401) {          
+        if (status === 401) {
           this.showAlert('You are not authorized for this!');
           this.logout();
         }
@@ -75,11 +75,11 @@ export class AuthService {
       })
     )
   }
- 
+
   isAuthenticated() {
     return this.authenticationState.value;
   }
- 
+
   showAlert(msg) {
     let alert = this.alertController.create({
       message: msg,
